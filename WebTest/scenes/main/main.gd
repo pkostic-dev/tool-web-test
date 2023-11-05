30a2a13bf4afbcbe95d1dc75ad07413559498074
Predrag Kostic <23319698+pkostic-dev@users.noreply.github.com>
2023-11-06 00:57:07 +0100

    Add particles

--- WebTest/scenes/main/main.gd
+++ WebTest/scenes/main/main.gd
@@ -4,7 +4,7 @@ var scene_2d_test = load("res://scenes/scene_2d/2d_test.tscn") as PackedScene
 var scene_3d_test = load("res://scenes/scene_3d/3d_test.tscn") as PackedScene
 var scene_settings = load("res://scenes/settings/settings.tscn") as PackedScene
 var scene_shaders = load("res://scenes/shaders/shaders.tscn") as PackedScene
-var scene_cpu_particles = load("res://scenes/cpu_particles/cpu_particles.tscn") as PackedScene
+var scene_gpu_particles = load("res://scenes/gpu_particles/gpu_particles.tscn") as PackedScene
 
 func _on_2d_test_pressed():
 	get_tree().change_scene_to_packed(scene_2d_test)
@@ -22,5 +22,5 @@ func _on_shaders_pressed():
 	get_tree().change_scene_to_packed(scene_shaders)
 
 
-func _on_cpu_particles_pressed():
-	get_tree().change_scene_to_packed(scene_cpu_particles)
+func _on_gpu_particles_pressed():
+	get_tree().change_scene_to_packed(scene_gpu_particles)
